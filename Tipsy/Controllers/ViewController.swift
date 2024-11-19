e7a604303108c9d3c07d7d7bde7444020f217185
eddie <eddie605134@gmailcom>
2024-11-19 16:55:57 +0800

    completed

---
+++ Tipsy/Controllers/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var billTextField: UITextField!
+	
+	@IBOutlet weak var tenPctBtn: UIButton!
+	@IBOutlet weak var zeroPctBtn: UIButton!
+	@IBOutlet weak var twentyPctBtn: UIButton!
+	@IBOutlet weak var spiltNumberText: UILabel!
+	
+	
+	@IBAction func tipChange(_ sender: UIButton) {
+	}
+	
+	@IBAction func stepperValueChange(_ sender: Any) {
+	}
+	
+	@IBAction func calculatePressed(_ sender: UIButton) {
+	}
+	
+	
+
+}
+
